b3088c212fece9fae2230f56ffd8ea51935b9800
Андрей <@DESKTOP-USER>
2022-09-19 15:03:56 +0400

    Добавьте файлы проекта.

---
+++ Console78-3/Program.cs
@@ -0,0 +1,13 @@
+﻿internal class Program
+{
+    private static void Main(string[] args)
+    {
+        Console.WriteLine("Один из крупнейших городов России");
+
+        string[] cities_array = new string[] { "Москва", "Санкт-Петербург", "Новосибирск", "Екатеринбург", "Нижний Новгород", "Казань" };
+        Random rand = new Random();
+        int index = rand.Next(0,10);
+
+        Console.WriteLine(cities_array[index]);
+    }
+}
